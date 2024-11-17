1176908e74104ecae44ac8a426c170aa45f27d37
Duke <commits@dmail.icu>
2024-11-17 10:50:03 +0100

    Initial commit

---
+++ TUnitCoverage.Tests/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace TUnitCoverage.Tests;
+
+public class Test1
+{
+	[Test]
+	public async Task TestMethod1()
+	{
+		var result = AdditionFunctions.Add(1, 1);
+		await Assert.That(result).IsEqualTo(2);
+	}
+}
